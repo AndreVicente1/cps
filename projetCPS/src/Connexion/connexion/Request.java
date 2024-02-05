ae9d0a575e3c26b1b8b726dbf3e55587dd505348
Andre Vicente <andre.vicente@etu.sorbonne-universite.fr>
2024-02-05 16:49:40 +0100

    salutgit statusgit status

---
+++ projetCPS/src/Connexion/connexion/Request.java
@@ -0,0 +1,42 @@
+package connexion;
+import fr.sorbonne_u.cps.sensor_network.interfaces.ConnectionInfoI;
+import fr.sorbonne_u.cps.sensor_network.interfaces.RequestI;
+import fr.sorbonne_u.cps.sensor_network.registry.interfaces.LookupCI;
+import fr.sorbonne_u.cps.sensor_network.requests.interfaces.QueryI;
+
+
+public class Request implements RequestI{
+	//p11
+	private ConnectionInfoI co;
+	public static String URI;
+	private QueryI q;
+	private boolean isAsynchronous;
+	
+	public Request(boolean isAsynchronous, String URI, QueryI query) {
+		this.isAsynchronous = isAsynchronous;
+		this.URI = URI;
+		this.q = query;
+	}
+	
+	@Override
+	public String requestURI() {
+		return URI;
+	}
+
+	@Override
+	public QueryI getQueryCode() { //to execute the request
+		return q;
+	}
+
+	@Override
+	public boolean isAsynchronous() {
+		return isAsynchronous;
+	}
+
+	@Override
+	public ConnectionInfoI clientConnectionInfo() {
+		
+		return co;
+	}
+
+}
---
+++ projetCPS/src/Connexion/connexion/Requesting.java
@@ -0,0 +1,23 @@
+package connexion;
+
+import fr.sorbonne_u.cps.sensor_network.interfaces.QueryResultI;
+import fr.sorbonne_u.cps.sensor_network.interfaces.RequestI;
+import fr.sorbonne_u.cps.sensor_network.nodes.interfaces.RequestingCI;
+
+public class Requesting implements RequestingCI{
+	public Requesting () { 
+		
+	}
+	@Override
+	public QueryResultI execute(RequestI request) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void executeAsync(RequestI request) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

db158061d2bbd8bff55dbd3d3d3ed71fdc5f4e85
luantoine <luluantoinex@gmail.com>
2024-02-18 16:39:46 +0100

    supprime doublons fichiers

--- projetCPS/src/Connexion/connexion/NodeI.java
+++
@@ -1,7 +0,0 @@
-package Connexion.connexion;
-
-import fr.sorbonne_u.cps.sensor_network.interfaces.NodeInfoI;
-import fr.sorbonne_u.cps.sensor_network.requests.interfaces.ProcessingNodeI;
-
-public interface NodeI extends NodeInfoI,ProcessingNodeI {
-}
